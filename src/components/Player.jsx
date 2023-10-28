78b992588c8c4d16cbe3ce06eeabd14526203f2a
Yury <matatov1989@gmail.com>
2023-10-28 19:04:25 +0300

    fix user input state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -12,12 +12,10 @@ export default function Player({ initialName, symbol }) {
         setPlayerName(event.target.value);
     }
 
-    let editablePlayerName = <span className="player-name">{playerName}</span>;
-    let btnCaption = 'Edit';
+    let editablePlayerName = (<span className="player-name">{playerName}</span>);
     
     if(isEditing) {
-        editablePlayerName = <input type='text' required value={playerName} onChange={handleChange} />;
-        btnCaption = 'Save';
+        editablePlayerName = (<input type='text' required value={playerName} onChange={handleChange} />);
     }
 
     return (
@@ -26,7 +24,7 @@ export default function Player({ initialName, symbol }) {
                 {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>{btnCaption}</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 }
